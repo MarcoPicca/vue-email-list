4e84077829aecd9625dba4701aa074ead989cb8d
MarcoPicca <il96marco@gmail.com>
2023-12-12 17:38:11 +0100

    stampando l'array riesco, in una lista non ancora

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const { createApp } = Vue
+
+
+createApp({
+    data() {
+        return {
+            newEmail: '',
+            newEmails: [],
+        }
+    },
+
+    methods: {
+        getNewEmail(){
+            for (let i=0; i < 10; i++) {
+                
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(
+                    (response) => {
+                        console.log(response);
+                        this.newEmails.push(response.data.response);
+                    });
+                
+            }
+        },
+
+    },
+    
+    created(){
+        this.getNewEmail();
+    },
+
+}).mount('#app');
